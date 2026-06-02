5535bcbff6f5d6e0077d0d386a38a692aaaff353
MathheusRamos <matheus.ramos@hinovamobile.com.br>
2026-06-01 21:38:24 -0300

    inicio
    inicio

---
+++ app/src/main/java/com/app/supercompras/MainActivity.kt
@@ -0,0 +1,234 @@
+package com.app.supercompras
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.supercompras.ui.theme.SuperComprasTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import com.app.supercompras.ui.theme.Coral
+import com.app.supercompras.ui.theme.Marinho
+import com.app.supercompras.ui.theme.Typography
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperComprasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ListaDeCompras(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListaDeCompras(modifier: Modifier = Modifier) {
+    var listaDeItens by rememberSaveable() { mutableStateOf(listOf<ItemCompra>()) }
+
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        LogoTopo()
+        AdicionarItem(onSalvarItem = { textoNovo ->
+            listaDeItens = listaDeItens + ItemCompra(textoNovo)
+        })
+        Spacer(modifier = Modifier.height(48.dp))
+        Titulo(
+            texto = "Lista de Compras",
+        )
+        Column {
+            listaDeItens.forEach { item ->
+                ItemDaLista(item)
+            }
+        }
+        Titulo(texto = "Comprados")
+    }
+}
+
+@Composable
+fun AdicionarItem(onSalvarItem: (texto: String) -> Unit, modifier: Modifier = Modifier) {
+    var texto = rememberSaveable { mutableStateOf("") }
+    OutlinedTextField(
+        value = texto.value,
+        onValueChange = { texto.value = it },
+        placeholder = {
+            Text(
+                text = "Digite o item que deseja adicionar",
+                color = Color.Gray,
+                style = Typography.bodyMedium
+            )
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        singleLine = true,
+        shape = RoundedCornerShape(24.dp)
+    )
+
+    Button(
+        shape = RoundedCornerShape(24.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Coral,
+        ),
+        onClick = {
+            onSalvarItem(texto.value)
+            texto.value = ""
+        },
+        modifier = modifier
+    ) {
+        Text(
+            text = "Salvar item",
+            color = Color.White,
+            style = Typography.bodyLarge,
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp)
+        )
+    }
+}
+
+@Composable
+fun Titulo(texto: String, modifier: Modifier = Modifier) {
+    Text(text = texto, modifier = modifier, style = Typography.headlineLarge)
+}
+
+@Composable
+fun ItemDaLista(item: ItemCompra, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Top, modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = {},
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .requiredSize(24.dp)
+            )
+            Text(
+                text = item.texto,
+                modifier = Modifier.weight(1f),
+                style = Typography.bodyMedium,
+                textAlign = TextAlign.Start
+            )
+            Icone(
+                Icons.Default.Delete,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(16.dp)
+            )
+            Icone(
+                Icons.Default.Edit,
+                modifier = Modifier
+                    .size(16.dp)
+            )
+
+        }
+        Text(
+            text = "Segunda-feira (31/10/2022) às 08:30",
+            Modifier.padding(top = 8.dp),
+            style = Typography.labelSmall
+        )
+    }
+}
+
+@Composable
+fun LogoTopo(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.logoapp),
+        contentDescription = null,
+        modifier = modifier.size(160.dp)
+    )
+}
+
+@Composable
+fun Icone(icone: ImageVector, modifier: Modifier = Modifier) {
+    Icon(icone, contentDescription = "Editar", modifier = modifier, tint = Marinho)
+}
+
+@Preview
+@Composable
+private fun AdicionarItemPreview() {
+    SuperComprasTheme {
+        AdicionarItem(onSalvarItem = {})
+    }
+}
+
+@Preview
+@Composable
+private fun IconePreview() {
+    SuperComprasTheme() {
+        Icone(icone = Icons.Default.Delete)
+    }
+}
+
+@Preview
+@Composable
+private fun ItemDaListaPreview() {
+    SuperComprasTheme {
+        ItemDaLista(item = ItemCompra("Suco"))
+    }
+}
+
+@Preview
+@Composable
+private fun LogoTopoPreview() {
+    SuperComprasTheme() {
+        LogoTopo()
+    }
+}
+
+@Preview
+@Composable
+private fun TituloPreview() {
+    SuperComprasTheme() {
+        Titulo(texto = "Lista de Compras")
+    }
+}
+
+data class ItemCompra(
+    val texto: String
+)
---
+++ app/src/main/java/com/app/supercompras/ui/theme/Type.kt
@@ -0,0 +1,51 @@
+package com.app.supercompras.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.toFontFamily
+import com.app.supercompras.R
+
+// Set of Material typography styles to start with
+val kronaFontFamily = Font(R.font.kronaone).toFontFamily()
+val numansFontFamily = Font(R.font.numans).toFontFamily()
+
+val Typography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = kronaFontFamily,
+        fontWeight = FontWeight.Normal,
+        color = Marinho,
+        fontSize = 20.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = numansFontFamily,
+        fontWeight = FontWeight.Normal,
+        color = Marinho,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = numansFontFamily,
+        fontWeight = FontWeight.Normal,
+        color = Marinho,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = numansFontFamily,
+        fontWeight = FontWeight.Normal,
+        color = Marinho,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    )
+)
